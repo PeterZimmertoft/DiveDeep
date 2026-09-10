94e38f2e541658ceac2fc948f54b4ce1ff15c214
Mathias Sørensen <mathiassrensen514@gmail.com>
2026-09-10 13:36:27 +0200

    Init2

--- DiveDeepWebApp/DiveDeepWebApp/Migrations/20260910113413_Init2.Designer.cs
+++ DiveDeepWebApp/DiveDeepWebApp/Migrations/20260910113413_Init2.Designer.cs
@@ -3,6 +3,7 @@ using DiveDeepWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DiveDeepWebApp.Migrations
 {
     [DbContext(typeof(DiveDeepContext))]
-    partial class DiveDeepContextModelSnapshot : ModelSnapshot
+    [Migration("20260910113413_Init2")]
+    partial class Init2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DiveDeepWebApp/DiveDeepWebApp/Migrations/20260910113413_Init2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiveDeepWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Fins",
+                keyColumn: "Id",
+                keyValue: 136,
+                column: "Size",
+                value: "XL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Fins",
+                keyColumn: "Id",
+                keyValue: 136,
+                column: "Size",
+                value: "L");
+        }
+    }
+}
