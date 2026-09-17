a62aacf79d391bff6b792da5085f08e384102609
Mathias Sørensen <mathiassrensen514@gmail.com>
2026-09-17 13:10:14 +0200

    packag

--- DiveDeepWebApp/DiveDeepWebApp/ViewModels/ProductViewModel.cs
+++ DiveDeepWebApp/DiveDeepWebApp/ViewModels/ProductViewModel.cs
@@ -1,28 +1,11 @@
-﻿using DiveDeepWebApp.Models;
+using DiveDeepWebApp.Models;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
 
 namespace DiveDeepWebApp.ViewModels
 {
-    public class ProductViewModel
+    public class ProductViewModel : PackageProductViewModel
     {
-        [ValidateNever]
-        public List<Product> Variants { get; set; }
-
-        [ValidateNever]
-        public string? BookingError { get; set; }
-
-        public string ProductName { get; set;  }
-
-        [Required(ErrorMessage = "Der skal vælges en størrelse!")]
-        public string? Size { get; set; }
-
-        [Required(ErrorMessage = "Der skal vælges en tykkelse!")]
-        public string? Thickness { get; set; }
-
-        [Required(ErrorMessage = "Der skal vælges et køn!")]
-        public string? Gender { get; set; }
-
         [Required(ErrorMessage = "Der skal vælges en startdato!")]
         public DateTime? StartDate { get; set; }
 
