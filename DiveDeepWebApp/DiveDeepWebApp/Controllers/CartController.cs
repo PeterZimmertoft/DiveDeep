d1f0b3299b794b379dece545a2f8b1889df5b6ef
Magnus Bonne Nielstedt <magnus.bonne@gmail.com>
2026-09-19 17:45:18 +0200

    Fixed: booking of multiple identical products with thier own quantity of 1

--- DiveDeepWebApp/DiveDeepWebApp/Controllers/CartController.cs
+++ DiveDeepWebApp/DiveDeepWebApp/Controllers/CartController.cs
@@ -81,6 +81,25 @@ namespace DiveDeepWebApp.Controllers
             }
 
             List<CartItem> cartItems = cartViewModel.Items.SelectMany(item => item.Products).ToList();
+            
+            //Kopi af cartItems til at få nulstillet kurven rigtigt.
+            List<CartItem> cartItemsForDeletion = cartItems.ToList();
+
+            // handtering af hvis 2 identiske produkter er i samme kurv, men det ikke fremgør af deres Quantity
+            // f.eks. det ene produkt er en del af en pakke, og det andet produkt er et enkelt ståede produkt
+            for (int i=0; i < cartItems.Count-1; i++)
+            {
+                for (int j=i+1; j < cartItems.Count; j++)
+                {
+                    if (cartItems[i].ProductId == cartItems[j].ProductId)
+                    {
+                        cartItems[i].Quantity += 1;
+                        cartItems.RemoveAt(j);
+                    }
+                }
+            }
+            //
+
             List<BookingProduct> bookingProducts = cartItems
                 .Where(item => item.ProductId != null)
                 .Select(item => new BookingProduct
@@ -117,7 +136,7 @@ namespace DiveDeepWebApp.Controllers
             };
     
             bookingRepository.Create(booking);
-            cartService.Delete(cartItems.Select(item => item.Id).ToList());
+            cartService.Delete(cartItemsForDeletion.Select(item => item.Id).ToList());
 
             return RedirectToAction(nameof(BookingsController.Index), "Bookings");  
         }
